4aa0c850f71225c772789197b14cb84aa41d1cfc
Samuele Benassai <77284041+tucanofasano@users.noreply.github.com>
2026-07-28 00:30:31 +0200

    Implemented + initial tests parsing encode and decode

--- addons/fw_saving_system/logic/parse/fw_binary_parser.gd
+++ addons/fw_saving_system/logic/parse/fw_binary_parser.gd
@@ -15,9 +15,15 @@ var game_version: StringName
 var godot_version: StringName
 
 func _init(game_literal: StringName, game_version: StringName, godot_version: StringName):
-	self.game_literal = game_literal.left(GAME_LITERAL_SIZE)
-	self.game_version = game_version.left(GAME_VERSION_SIZE)
-	self.godot_version = godot_version.left(GODOT_VERSION_SIZE)
+	self.game_literal = _crop_string(game_literal)
+	self.game_version = _crop_string(game_version)
+	self.godot_version = _crop_string(godot_version)
+
+func _crop_string(original: String, size: int = 31):
+	var bytes: PackedByteArray = original.to_utf8_buffer()
+	if bytes.size() <= size:
+		return original
+	return bytes.slice(0, size).get_string_from_utf8()
 
 func decode(bytes: PackedByteArray) -> FWSaveTypes.Result:
 	var file_data := FWSaveTypes.FileData.new()
@@ -73,16 +79,16 @@ func _decode_v1_header(stream: StreamPeerBuffer, file_data: FWSaveTypes.FileData
 	file_data.header.toc_count = stream.get_u16()
 	file_data.header.flags = stream.get_u16()
 	
-	file_data.header.game_literal = stream.get_string(32)
+	file_data.header.game_literal = stream.get_utf8_string(32)
 	if game_literal != self.game_literal:
 		return FWSaveTypes.Result.failure(FWSaveTypes.Error.DIFFERENT_GAME_LITERAL)
 	stream.seek(48)
 	
-	file_data.header.game_version = stream.get_string(32)
+	file_data.header.game_version = stream.get_utf8_string(32)
 	stream.seek(80)
 	
 	# Godot Version is currently skipped
-	file_data.header.godot_version = stream.get_string(32)
+	file_data.header.godot_version = stream.get_utf8_string(32)
 	stream.seek(112)
 	
 	stream.seek(192)
@@ -109,7 +115,7 @@ func _decode_v1_toc(stream: StreamPeerBuffer, file_data: FWSaveTypes.FileData) -
 	for i in range(file_data.header.toc_count):
 		var toc_entry: FWSaveTypes.TocEntry = FWSaveTypes.TocEntry.new()
 		
-		toc_entry.key = stream.get_string(32)
+		toc_entry.key = stream.get_utf8_string(32)
 		stream.seek(256 + 80 * i + 32)
 		
 		toc_entry.payload_offset = stream.get_u32()
@@ -141,16 +147,118 @@ func _encode_v1(file_data: FWSaveTypes.FileData) -> FWSaveTypes.Result:
 	var buffer: PackedByteArray = PackedByteArray()
 	buffer.resize(size)
 	
+	var stream: StreamPeerBuffer = StreamPeerBuffer.new()
+	stream.big_endian = false
+	stream.data_array = buffer
+
+	file_data.header.file_size = size
+
+	var res: FWSaveTypes.Result = _encode_v1_payloads(file_data, stream)
+	if not res.is_ok():
+		return res
+	res = _encode_v1_toc(file_data, stream)
+	if not res.is_ok():
+		return res
+	res = _encode_v1_header(file_data, stream)
+	if not res.is_ok():
+		return res
+
+	return FWSaveTypes.Result.success(stream.data_array)
+
+func _encode_v1_payloads(file_data: FWSaveTypes.FileData, stream: StreamPeerBuffer) -> FWSaveTypes.Result:
+	stream.seek(256 + 80 * file_data.toc.size())
+
+	for key in file_data.payloads:
+		if not key in file_data.toc:
+			FWSaveTypes.Result.failure(FWSaveTypes.Error.TOC_ENTRIES_NOT_PRESENT)
+		file_data.toc[key].payload_offset = stream.get_position()
+		
+		var err = stream.put_data(file_data.payloads[key])
+		if err != OK:
+			FWSaveTypes.Result.failure(FWSaveTypes.Error.ENCODED_PAYLOAD_STREAMING_ERROR)
 	
+	return FWSaveTypes.Result.success(file_data)
+
+func _encode_v1_toc(file_data: FWSaveTypes.FileData, stream: StreamPeerBuffer):
+	var ctx = HashingContext.new()
+	stream.seek(256)
+
+	for key in file_data.toc:
+		if key.to_utf8_buffer().size() > 31:
+			FWSaveTypes.Result.failure(FWSaveTypes.Error.INVALID_KEY_LENGTH)
+
+		var pos: int = stream.get_position()
+		var entry: FWSaveTypes.TocEntry = file_data.toc[key]
+
+		if key in file_data.payloads:
+			var payload: PackedByteArray = file_data.payloads[key]
+			entry.payload_disk_size = payload.size()
+			
+			ctx.start(HashingContext.HASH_SHA256)
+			ctx.update(payload)
+			var checksum: PackedByteArray = ctx.finish()
+			entry.payload_checksum = checksum
+
+		var err: int = stream.put_data(key.to_utf8_buffer())
+		if err != OK:
+			FWSaveTypes.Result.failure(FWSaveTypes.Error.ENCODED_TOC_KEY_STREAMING_ERROR)
+		stream.seek(pos + 32)
+		stream.put_u32(entry.payload_offset)
+		stream.put_u32(entry.payload_disk_size)
+		stream.put_u32(entry.payload_uncompressed_size)
+		stream.put_u16(entry.encoding)
+		stream.put_u16(entry.flags)
+		err = stream.put_data(entry.payload_checksum)
+		if err != OK:
+			FWSaveTypes.Result.failure(FWSaveTypes.Error.ENCODED_PAYLOAD_CHECKSUM_STREAMING_ERROR)
 	
-	return FWSaveTypes.Result.failure(100)
+	return FWSaveTypes.Result.success(file_data)
 
 func _encode_v1_header(file_data: FWSaveTypes.FileData, stream: StreamPeerBuffer):
+	var ctx = HashingContext.new()
+	stream.seek(0)
+
 	file_data.header.magic_number = MAGIC_NUMBER
 	file_data.header.version = 1
+	file_data.header.toc_count = file_data.toc.size()
 
-func _encode_v1_toc():
-	pass
+	file_data.header.game_literal = game_literal
+	file_data.header.game_version = game_version
+	file_data.header.godot_version = godot_version
 
-func _encode_v1_payloads():
-	pass
+	stream.put_u32(file_data.header.magic_number)
+	stream.put_u32(file_data.header.version)
+	stream.put_u32(file_data.header.file_size)
+	stream.put_u16(file_data.header.toc_count)
+	stream.put_u16(file_data.header.flags)
+
+	var err: int = stream.put_data(file_data.header.game_literal.to_utf8_buffer())
+	if err != OK:
+		FWSaveTypes.Result.failure(FWSaveTypes.Error.ENCODED_GAME_LITERAL_STREAMING_ERROR)
+	stream.seek(48)
+	err = stream.put_data(file_data.header.game_version.to_utf8_buffer())
+	if err != OK:
+		FWSaveTypes.Result.failure(FWSaveTypes.Error.ENCODED_GAME_VERSION_STREAMING_ERROR)
+	stream.seek(80)
+	err = stream.put_data(file_data.header.godot_version.to_utf8_buffer())
+	if err != OK:
+		FWSaveTypes.Result.failure(FWSaveTypes.Error.ENCODED_GODOT_VERSION_STREAMING_ERROR)
+	stream.seek(112)
+
+	stream.seek(192)
+
+	ctx.start(HashingContext.HASH_SHA256)
+	ctx.update(stream.data_array.slice(256, 256 + 80 * file_data.header.toc_count))
+	file_data.header.toc_checksum = ctx.finish()
+	err = stream.put_data(file_data.header.toc_checksum)
+	if err != OK:
+		FWSaveTypes.Result.failure(FWSaveTypes.Error.ENCODED_TOC_CHECKSUM_STREAMING_ERROR)
+
+	ctx.start(HashingContext.HASH_SHA256)
+	ctx.update(stream.data_array.slice(0, 224))
+	file_data.header.header_checksum = ctx.finish()
+	err = stream.put_data(file_data.header.header_checksum)
+	if err != OK:
+		FWSaveTypes.Result.failure(FWSaveTypes.Error.ENCODED_HEADER_CHECKSUM_STREAMING_ERROR)
+
+	return FWSaveTypes.Result.success(file_data)
--- test/main.gd
+++ test/main.gd
@@ -3,23 +3,31 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(FWBinaryParser.MAGIC_NUMBER)
-	print(FWSaveTypes.Result.failure(1000))
-	
-	var bytes = FileAccess.get_file_as_bytes("res://test/test.bin")
-	var parser = FWBinaryParser.new("a", "b", "c")
-	var data = parser.decode(bytes).value as FWSaveTypes.FileData
-	print(data.header.version)
-	print(data.header.file_size)
-	print(data.header.toc_count)
-	print(data.header.flags)
-	print(data.header.game_literal)
-	print(data.header.game_version)
-	print(data.header.godot_version)
-	print(data.header.toc_checksum)
-	print(data.header.header_checksum)
+	# print(FWBinaryParser.MAGIC_NUMBER)
+	# print(FWSaveTypes.Result.failure(2000 as FWSaveTypes.Error))
+	# var bytes = FileAccess.get_file_as_bytes("res://test/test.bin")
+	# var parser = FWBinaryParser.new("a", "b", "c")
+	# var data = parser.decode(bytes).value as FWSaveTypes.FileData
+	# print(data.header.version)
+	# print(data.header.file_size)
+	# print(data.header.toc_count)
+	# print(data.header.flags)
+	# print(data.header.game_literal)
+	# print(data.header.game_version)
+	# print(data.header.godot_version)
+	# print(data.header.toc_checksum)
+	# print(data.header.header_checksum)
+	var data: FWSaveTypes.FileData = FWSaveTypes.FileData.new()
+	data.toc[&"key1"] = FWSaveTypes.TocEntry.new()
+	data.payloads[&"key1"] = PackedByteArray([1,2,3,4,5])
+
+	var parser := FWBinaryParser.new("MyGame1", "1.0.0", "godot4.5.1")
+	var bytes := parser.encode(data).value as PackedByteArray
+	FileAccess.open("res://test/out.fwsv", FileAccess.WRITE).store_buffer(bytes)
+
+	print(bytes)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
